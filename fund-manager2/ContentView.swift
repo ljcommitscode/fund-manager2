9d98e12afb58eb9e15ff09b3bbc7d8f1148f4573
LJ Cutler <chillidoglj@icloud.com>
2026-07-23 11:30:54 -0400

    Initial Commit

---
+++ fund-manager2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fund-manager2
+//
+//  Created by Leon Cutler on 7/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fund-manager2/fund_manager2App.swift
@@ -0,0 +1,17 @@
+//
+//  fund_manager2App.swift
+//  fund-manager2
+//
+//  Created by Leon Cutler on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct fund_manager2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
