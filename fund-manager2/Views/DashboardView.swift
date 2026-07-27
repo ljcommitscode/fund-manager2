fbe60937996d19de6b28d51956d189503bbe3f0b
LJ Cutler <chillidoglj@icloud.com>
2026-07-27 10:45:52 -0400

    First Version

---
+++ fund-manager2/Views/DashboardView.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard.swift
+//  fund-manager2
+//
+//  Created by Leon Cutler on 7/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Text("hello world")
+        }
+    }
+}
